ca3401aa1e981d27e4543ebb5928882819ea4ebf
emersonmarcantuani <emerson83@orange.fr>
2024-06-16 11:49:31 +0200

    Nettoyage des fichiers non front-end de la branche front-end

--- config/baseDeDonnees.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize'); // Importation de la classe Sequelize depuis le module sequelize
-
-const sequelize = new Sequelize({ // Création d'une instance de Sequelize
-    dialect: 'sqlite', // Utilisation du dialecte SQLite
-    storage: './database.sqlite' // Stockage de la base de données dans un fichier database.sqlite
-});
-
-module.exports = sequelize; // Exportation de l'instance de Sequelize
--- modeles/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../config/baseDeDonnees'); // Importation de l'instance de Sequelize depuis le fichier config/baseDeDonnees
-const Blague = require('./blague'); // Importation du modèle Blague depuis le fichier modeles/blague
-
-const initDB = async () => { // Définition de la fonction initDB pour initialiser la base de données
-    await sequelize.sync({ force: true }); // Synchronisation de la base de données avec les modèles (force: true pour recréer les tables à chaque démarrage)
-    console.log("Base de données synchronisée"); // Affichage d'un message dans la console pour indiquer que la base de données a été synchronisée avec succès
-};
-
-module.exports = { initDB, Blague }; // Exportation de la fonction initDB et du modèle Blague pour pouvoir les utiliser dans d'autres fichiers
