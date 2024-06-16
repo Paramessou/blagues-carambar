35b5ba9cb5e2555d75b59e58eb5aee5cc5a0a77e
emersonmarcantuani <emerson83@orange.fr>
2024-06-16 22:12:05 +0200

    Maj initialisation base de données

--- config/baseDeDonnees.js
+++ config/baseDeDonnees.js
@@ -5,4 +5,13 @@ const sequelize = new Sequelize({ // Création d'une instance de Sequelize
     storage: './database.sqlite' // Stockage de la base de données dans un fichier database.sqlite
 });
 
-module.exports = sequelize; // Exportation de l'instance de Sequelize
+const initDB = async () => { // Définition de la fonction initDB pour initialiser la base de données SQLite
+    try { // Gestion des erreurs avec try...catch
+        await sequelize.authenticate(); // Authentification à la base de données SQLite (vérifie si la connexion est établie)
+        console.log('Connexion établit avec succès.');
+    } catch (error) {
+        console.error('Impossible de se connecter à la base de données:', error);
+    }
+};
+
+module.exports = { sequelize, initDB }; // Exportation de l'instance de Sequelize et de la fonction d'initialisation
