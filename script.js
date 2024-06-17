75f46df5bbddeaa38db977ff6ce9de2e46703e84
Paramessou <emerson83@orange.fr>
2024-06-17 23:17:54 +0200

    maj de script.js ajout message erreur

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-document.getElementById('boutonBlague').addEventListener('click', async () => { // Ajoute un écouteur d'événement sur le bouton avec l'ID 'boutonBlague'
-    const response = await fetch('https://blagues-carambar.onrender.com/api/blagues/aleatoire'); // Envoie une requête GET à l'URL spécifiée
-    const blague = await response.json(); // Convertit la réponse en objet JavaScript (ici, un objet blague)
-    document.getElementById('affichageBlague').innerText = `${blague.contenu} - ${blague.chute}`; // Affiche la blague dans la page web
+document.getElementById('boutonBlague').addEventListener('click', async () => {
+    try {
+        const response = await fetch('https://blagues-carambar.onrender.com/api/blagues/aleatoire');
+        if (!response.ok) {
+            throw new Error('La réponse du réseau n\'est pas correcte');
+        }
+        const blague = await response.json();
+        document.getElementById('affichageBlague').innerText = `${blague.contenu} - ${blague.chute}`;
+    } catch (error) {
+        console.error('Erreur lors de la récupération de la blague:', error);
+        document.getElementById('affichageBlague').innerText = 'Erreur lors de la récupération de la blague.';
+    }
 });
