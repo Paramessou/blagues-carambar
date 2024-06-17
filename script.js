a311a24fa21115d7b8b1502ae7560e1a831f4eab
emersonmarcantuani <emerson83@orange.fr>
2024-06-17 22:06:50 +0200

    Correction chemin accès blagues script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('boutonBlague').addEventListener('click', async () => { // Ajoute un écouteur d'événement sur le bouton avec l'ID 'boutonBlague'
-    const response = await fetch('https://blagues-carambar.onrender.com/api/blagues/aleatoire/'); // Envoie une requête GET à l'URL spécifiée
+    const response = await fetch('https://blagues-carambar.onrender.com/api/blagues/aleatoire'); // Envoie une requête GET à l'URL spécifiée
     const blague = await response.json(); // Convertit la réponse en objet JavaScript (ici, un objet blague)
     document.getElementById('affichageBlague').innerText = `${blague.contenu} - ${blague.chute}`; // Affiche la blague dans la page web
 });
