18e44be8a6b4537875bcc217d851bcf74dda299d
emersonmarcantuani <emerson83@orange.fr>
2024-06-18 08:02:19 +0200

    Retour au script.js original

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('boutonBlague').addEventListener('click', async () => {
     try {
-        const response = await fetch('https://blagues-carambar.onrender.com/api/blagues/');
+        const response = await fetch('https://blagues-carambar.onrender.com/api/blagues/aleatoire/'); // Appel de l'API pour récupérer une blague aléatoire depuis l'URL
         if (!response.ok) {
             throw new Error('La réponse du réseau n\'est pas correcte');
         }
-        const blague = await response.json();
-        document.getElementById('affichageBlague').innerText = `${blague.contenu} - ${blague.chute}`;
+        const blague = await response.json(); // Récupération de la blague sous forme d'objet JSON
+        document.getElementById('affichageBlague').innerText = `${blague.contenu} - ${blague.chute}`; // Affichage de la blague dans la page HTML avec le contenu et la chute de la blague récupérée depuis l'API
     } catch (error) {
         console.error('Erreur lors de la récupération de la blague:', error);
         document.getElementById('affichageBlague').innerText = 'Erreur lors de la récupération de la blague.';
